36bb2d795cd372fd1f50f5dd7b5f74d101e57060
Charles Fracchia <charlesfracchia@gmail.com>
2016-01-12 15:16:42 -0500

    Adding test

---
+++ testSerial.js
@@ -0,0 +1,29 @@
+var sp = require('serial');
+var serial = new sp.SerialPort("/dev/ttyAMA0",{
+  baudrate: 9600,
+  stopBits: 2
+});
+
+serial.open(function (error) {
+  if ( error ) {
+    console.log('>>> Failed to open serial port. Error: ' + error);
+  } else {
+    console.log('>>> Serial port open');
+  }
+});
+
+serial.write("T", function(err, results) {
+    if (err){
+      console.log('>>> Error writing status request packet:' + err);
+    }else{
+      console.log(">>> Sent status request packet (T)");
+    }
+});
+
+serial.on('data', function(error, data){
+  if (error) {
+    console.log('>>> Serial port data error: ' + error);
+  } else {
+    console.log('>>> Data received: ' + data.toString());
+  }
+});
