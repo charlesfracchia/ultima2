5a9da94b10f46f16e644aba73f4216b38d768aa5
Charles Fracchia <charlesfracchia@gmail.com>
2016-02-12 23:14:54 -0500

    Moved ultima2 to its own JS file. Changed the scale for the front end graph.

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ var moment = require('moment');
 var mqtt = require('mqtt');
 var fs = require('fs');
 var convertBase = require('./convertBase.js').convertBase;
+var ultima2 = require('./ultima2.js');
 
 var conf = {
   "rejectUnauthorized": false,
@@ -47,74 +48,6 @@ client.on('message', function (topic, message) {
 });
 /*******************END MQTT*******************/
 
-/*********************REVCO ULTIMA II*********************/
-function sendQuit (serial) {
-  serial.write("Q", function(err, results) {
-    if (err !== undefined){
-      console.log('err ' + err);
-    }else{
-      console.log(">>> Sent TELEM stop packet (Q)");
-    }
-  });
-}
-
-function getNVRAM(serial){
-  serial.write("T", function(err, results) {
-    if (err !== undefined){
-      console.log('err ' + err);
-    }else{
-      console.log(">>> Sent status request packet (T)");
-    }
-  });
-}
-
-function processData (data) {
-  var split = data.match(/.{1,4}/g);
-  var bytes = {
-    status: split[0],
-    temperature: split[1],
-    unknown: split[2],
-    setPoint: split[3]
-  };
-  bytes.temperature = processTemp(bytes.temperature);
-  bytes.status = processStatus(bytes.status);
-  bytes.setPoint = processTemp(bytes.setPoint);
-  var now = new Date();
-  addToLogFile(now+","+bytes.temperature, 'tempLog.txt');
-  return bytes;
-}
-
-function processTemp (tempBytes) {
-  var i = parseInt(tempBytes, 16);
-  var t = i / 129 - 243.7519;
-  return parseFloat(t).toFixed(2);
-}
-
-function processStatus (statusBytes) {
-  var sta = convertBase.hex2bin(statusBytes);
-  var diff = 16 - sta.length;
-  sta = "0".repeat(diff) + sta;
-
-  var alS = [];
-  if (sta[14] == 1) alS.push("high");
-  if (sta[15] == 1) alS.push("low");
-  if (sta[16] == 1) alS.push("RTD test");
-
-  var status = {
-    battery : parseInt(sta[0]),
-    power : parseInt(sta[1]),
-    fuse : parseInt(sta[4]),
-    filter : parseInt(sta[5]),
-    compressor : {
-      first : parseInt(sta[9]),
-      second : parseInt(sta[10])
-    },
-    alarm : alS
-  };
-  return status;
-}
-/*******************END REVCO ULTIMA II*******************/
-
 /*********************SERIAL*********************/
 //When serial is open
 serial.open(function (error) {
@@ -124,8 +57,8 @@ serial.open(function (error) {
     console.log('>>> Serial port open');
     serial.flush(function(){
       console.log(">>> Serial port flushed");
-      //sendQuit(serial);
-      getNVRAM(serial);
+      //ultima2.sendQuit(serial);
+      ultima2.getTPacket(serial);
     });
 
   }
@@ -170,7 +103,7 @@ function dataTimeoutCB (mBuffer) {
   }else if (mBuffer.length == 16){
     // console.log(">>> Have enough! L="+mBuffer.length);
     // console.log(mBuffer);
-    var processed = processData(mBuffer);
+    var processed = ultima2.processData(mBuffer);
     var now = new Date();
     //Push the latest values and timestamps correctly
     if (values.length < 2){
@@ -252,11 +185,10 @@ function addToLogFile (data, filepath) {
 
 /********************MAIN********************/
 var interval = setInterval(function (){
-  getNVRAM(serial);
+  ultima2.getTPacket(serial);
 }, refreshInterval);
 /******************END MAIN******************/
 
 /********************EXPORTS********************/
-module.exports.processData = processData;
 module.exports.getSlope = getSlope;
 /******************END EXPORTS******************/
---
+++ ultima2.js
@@ -0,0 +1,71 @@
+function sendQuit (serial) {
+  serial.write("Q", function(err, results) {
+    if (err !== undefined){
+      console.log('err ' + err);
+    }else{
+      console.log(">>> Sent TELEM stop packet (Q)");
+    }
+  });
+}
+
+function getTPacket(serial){
+  serial.write("T", function(err, results) {
+    if (err !== undefined){
+      console.log('err ' + err);
+    }else{
+      console.log(">>> Sent status request packet (T)");
+    }
+  });
+}
+
+function processData (data) {
+  var split = data.match(/.{1,4}/g);
+  var bytes = {
+    status: split[0],
+    temperature: split[1],
+    unknown: split[2],
+    setPoint: split[3]
+  };
+  bytes.temperature = processTemp(bytes.temperature);
+  bytes.status = processStatus(bytes.status);
+  bytes.setPoint = processTemp(bytes.setPoint);
+  var now = new Date();
+  addToLogFile(now+","+bytes.temperature, 'tempLog.txt');
+  return bytes;
+}
+
+function processTemp (tempBytes) {
+  var i = parseInt(tempBytes, 16);
+  var t = i / 129 - 243.7519;
+  return parseFloat(t).toFixed(2);
+}
+
+function processStatus (statusBytes) {
+  var sta = convertBase.hex2bin(statusBytes);
+  var diff = 16 - sta.length;
+  sta = "0".repeat(diff) + sta;
+
+  var alS = [];
+  if (sta[14] == 1) alS.push("high");
+  if (sta[15] == 1) alS.push("low");
+  if (sta[16] == 1) alS.push("RTD test");
+
+  var status = {
+    battery : parseInt(sta[0]),
+    power : parseInt(sta[1]),
+    fuse : parseInt(sta[4]),
+    filter : parseInt(sta[5]),
+    compressor : {
+      first : parseInt(sta[9]),
+      second : parseInt(sta[10])
+    },
+    alarm : alS
+  };
+  return status;
+}
+
+module.exports.sendQuit = sendQuit;
+module.exports.getTPacket = getTPacket;
+module.exports.processData = processData;
+module.exports.processTemp = processTemp;
+module.exports.processStatus = processStatus;
